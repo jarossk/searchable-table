fb36dd321cd0e9ca9d3eaabfc9dfd67cec9a8bff
Jarek <klata.jarek@gmail.com>
2018-05-15 16:05:12 +0100

    Added react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Users extends Component {
+      state = {
+        users: []
+      };
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="Card">
+      {this.state.users.map((user) =>
+            <div className="container" key={user.id}>
+              <img src={'https://www.w3schools.com/howto/img_avatar.png'} alt={"Avatar"} />
+              <h4><b>{user.username}</b></h4>
+              <p>{user.name}</p>
+            </div>)}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .then(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +55,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response}</p>
+        <Users />
       </div>
     );
   }
